a2acfd71160d123dfc08b9844a8b91e4a537341c
dseminara <darios3@gmail.com>
2012-09-29 15:00:37 -0300

    Loading texture from bitmap on filesystem

--- opengltest2/opengltest2.cpp
+++ opengltest2/opengltest2.cpp
@@ -13,6 +13,8 @@
 #include "glm/gtx/transform.hpp"
 #include <glm/gtc/type_ptr.hpp>
 
+#include "windows.h"
+
 int transform_matrix_index;
 
 #define wglewGetContext() (&_wglewctx)
@@ -181,35 +183,45 @@ void init_buffers() {
 
 GLuint textureid;
 
-char texture_data[100*100*4];
-
 void loadAndInitTexture(const char* filename, GLuint programHandle)
 {
-    // Load texture file
-//    QImage timg = QGLWidget::convertToGLFormat(QImage(filename,"JPG"));
-//    memset(texture_data,0x40,sizeof(texture_data));
-	for (int i=0; i<100; i++) {
-	for (int j=0; j<100; j++) {
-		texture_data[(j*100+i)*4] = j+i;
-		texture_data[(j*100+i)*4+1] = j+i;
-		texture_data[(j*100+i)*4+2] = 128;
-		texture_data[(j*100+i)*4+3] = 255;
 
-		if (i<10 && j<10) {
-		texture_data[(j*100+i)*4] = 128;
-		texture_data[(j*100+i)*4+1] = 0;
-		texture_data[(j*100+i)*4+2] = 0;
-		texture_data[(j*100+i)*4+3] = 255;
+   HBITMAP hBitmap = (HBITMAP)::LoadImage(NULL, "e:\\imagen.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE|LR_CREATEDIBSECTION);
+   if(NULL == hBitmap)
+   {
+      // LoadImage faled so get extended error information.
+	   throw std::runtime_error("error loading texture");
+   }
+
+    BITMAP bm;
+    ::GetObject( hBitmap , sizeof(bm) , &bm );
+
+	BITMAPINFO bmpInfo;
+    bmpInfo.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+    bmpInfo.bmiHeader.biWidth = bm.bmWidth;
+    bmpInfo.bmiHeader.biHeight = -bm.bmHeight;
+    bmpInfo.bmiHeader.biPlanes = 1;
+	bmpInfo.bmiHeader.biBitCount = bm.bmBitsPixel;
+    bmpInfo.bmiHeader.biCompression = BI_RGB;        
+    bmpInfo.bmiHeader.biSizeImage = 0;  
+
+	HDC dcBitmap = CreateCompatibleDC ( NULL );
+    SelectObject( dcBitmap, hBitmap );
+
+	//COLORREF* pixel = new COLORREF [ bm.bmWidth * bm.bmHeight ];
+	unsigned char* bitmap_data = (unsigned char*)malloc(bm.bmHeight * bm.bmWidth * 3);
+	unsigned char* texture_data = (unsigned char*)malloc(bm.bmHeight * bm.bmWidth * 4);
+
+	GetDIBits(dcBitmap, hBitmap, 0, bm.bmHeight, bitmap_data, &bmpInfo, DIB_RGB_COLORS );
+	int line_width = 2;
+	for (int i=0; i<bm.bmWidth; i++) {
+		for (int j=0; j<bm.bmHeight; j++) {
+			unsigned char* bitmapPixelData = (unsigned char*)bitmap_data + (j*bm.bmWidth + i) * 3 + j*2;
+			unsigned char* texturePixelData = (unsigned char*)texture_data + (j*bm.bmWidth + i) * 4;
+			texturePixelData[0] = bitmapPixelData[2];
+			texturePixelData[1] = bitmapPixelData[1];
+			texturePixelData[2] = bitmapPixelData[0];
 		}
-
-		if (i>90 && j>90) {
-		texture_data[(j*100+i)*4] = 0;
-		texture_data[(j*100+i)*4+1] = 128;
-		texture_data[(j*100+i)*4+2] = 0;
-		texture_data[(j*100+i)*4+3] = 255;
-		}
-
-	}
 	}
 
     // Copy file to OpenGL
@@ -217,7 +229,7 @@ void loadAndInitTexture(const char* filename, GLuint programHandle)
     glGenTextures(1, &textureid);
     glBindTexture(GL_TEXTURE_2D, textureid);
 
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 100, 100, 0, GL_RGBA, GL_UNSIGNED_BYTE, texture_data);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, bm.bmWidth, bm.bmHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, texture_data);
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     
