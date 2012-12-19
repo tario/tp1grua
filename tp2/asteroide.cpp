17236a95a1d01ee6fb70b016032a33b2945ce361
dseminara <darios3@gmail.com>
2012-12-19 13:25:36 -0300

    implemented textured material with phong for asteroid

--- tp2/asteroide.cpp
+++ tp2/asteroide.cpp
@@ -4,7 +4,8 @@
 #include "bitmap_texture.h"
 #include "segmento_recta.h"
 #include "curva_constante.h"
-
+#include "material_tp3.h" 
+#include "null_texture.h"
 #include <algorithm>
 
 Material* Asteroide::materialAsteroide = 0;
@@ -20,7 +21,7 @@ class ConjuntoPuntosCuerpoAsteroide : public FuncionConjuntoPuntos {
 			a.push_back(rand() % 1000 / 5000.0 + 0.1);
 			b.push_back(rand() % 1000 / 5000.0 + 0.1);
 			c.push_back(rand() % 1000 / 5000.0 + 0.1);
-			for (int i=1; i<16; i++){
+			for (int i=1; i<8; i++){
 				a.push_back(a.at(i-1) + rand() % 1000 / 5000.0 - 0.1);
 				b.push_back(b.at(i-1) + rand() % 1000 / 5000.0 - 0.1);
 				c.push_back(c.at(i-1) + rand() % 1000 / 5000.0 - 0.1);
@@ -29,25 +30,16 @@ class ConjuntoPuntosCuerpoAsteroide : public FuncionConjuntoPuntos {
 
 		std::vector<Punto> conjunto(float t) {
 			std::vector<Punto> ret;
-
-			//for (int i=0; i++; i<16) {
-			//	ret.push_back(Punto(float(rand() % 1000) / 1000.0 - 0.5, float(rand() % 1000) / 1000.0 - 0.5));
-			//}
-
-
-
-			for (int i=0; i<16; i++){
-				float t2 = i * 6.28/ 16;
+			for (int i=0; i<8; i++){
+				float t2 = i * 6.28/ 8;
 				float d = sqrt(0.25-(t-0.5)*(t-0.5));
 
 				if (rand() % 4 == 1) {
 					d = d * (rand() % 15 + 40) * 0.025;
 				}
 
-				ret.push_back(Punto( (cos(t2)+a[i] )*d*0.5, (sin(t2) + b[i])*d*0.5));
+				ret.push_back(Punto( (cos(t2)+a[i] )*d*0.5, (sin(t2) + b[i])*d*0.5, Punto::SMOOTH));
 			}
-
-			//std::sort(ret.begin(), ret.end(), comparador_puntos);
 
 			return ret;
 		};
@@ -60,7 +52,16 @@ class ConjuntoPuntosCuerpoAsteroide : public FuncionConjuntoPuntos {
 
 Asteroide::Asteroide() {
 	if (Asteroide::materialAsteroide==0) {
-		Asteroide::materialAsteroide = new MaterialTextura(new BitmapTexture("lunar.bmp"));
+		MaterialTP3* materialtp3 = new MaterialTP3(new BitmapTexture("lunar.bmp"), NullTexture::instance(),NullTexture::instance());
+		materialtp3->kaSetter->set(0.1);
+		materialtp3->kdSetter->set(0.9);
+		materialtp3->ksSetter->set(0.0);
+		materialtp3->intensidadDifusoSetter->set(1.0);
+		materialtp3->intensidadGrisSetter->set(0.0);
+		materialtp3->intensidadRelieveSetter->set(0.0);
+		materialtp3->intensidadReflexionSetter->set(0.0);
+
+		Asteroide::materialAsteroide = materialtp3;
 	}
 	CurvaConstante torcion(glm::vec3(0.0,1.0,0.0));
 	cuerpo = new Barrido(
