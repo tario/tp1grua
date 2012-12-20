3d5274639e5dc3929fe706acad7419af84d9c1b0
dseminara <darios3@gmail.com>
2012-12-20 18:45:59 -0300

    adjusted propulsion and engine shape parameters

--- tp2/propulsor.cpp
+++ tp2/propulsor.cpp
@@ -10,7 +10,7 @@ class ConjuntoPuntosPropulsorPrisma1 : public FuncionConjuntoPuntos {
 		std::vector<Punto> ret;
 		for (int i=0;i<16;i++) {
 			float angle = float(i)*6.28/16;
-			float r=0.10;
+			float r=0.095;
 			ret.push_back(Punto(cos(angle)*r,sin(angle)*r, Punto::SMOOTH));
 		}
 		return ret;
@@ -22,10 +22,10 @@ class ConjuntoPuntosPropulsorCoberturaPrisma : public FuncionConjuntoPuntos {
 	std::vector<Punto> conjunto(float t) {
 		std::vector<Punto> ret;
 		float l = 0.15;
-		ret.push_back(Punto(-0.01,0.0));
-		ret.push_back(Punto(0.01,0.0));
-		ret.push_back(Punto(0.01,-l));
-		ret.push_back(Punto(-0.01,-l));
+		ret.push_back(Punto(-0.015,0.0));
+		ret.push_back(Punto(0.015,0.0));
+		ret.push_back(Punto(0.015,-l));
+		ret.push_back(Punto(-0.015,-l));
 		return ret;
 	}
 };
@@ -33,7 +33,7 @@ class ConjuntoPuntosPropulsorCoberturaPrisma : public FuncionConjuntoPuntos {
 Propulsor::Propulsor(Material* material) : material(material), materialColorSolido(glm::vec3(0.0,1.0,1.0),false) {
 	propulsorPrisma2 = new Barrido(
 		&ConjuntoPuntosPropulsorPrisma1(), 
-		&SegmentoRecta(glm::vec3(0.0,0.0,0.0),glm::vec3(-0.1,0.0,0.0)),
+		&SegmentoRecta(glm::vec3(0.0,0.0,0.0),glm::vec3(-0.2,0.0,0.0)),
 		&CurvaConstante(glm::vec3(-1.0,0.0,0.0)),
 		&CurvaConstante(glm::vec3(0.0,1.0,0.0)),
 		1.0,
